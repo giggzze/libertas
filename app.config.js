bc440154b75ec036d2faca44c39cff52da6cc306
giggzze <giggzze@live.co.uk>
2025-05-28 19:27:40 +0100

    refactor(config): update app configuration for improved structure and clarity
    - Reformatted app.config.js for better readability and consistency.
    - Added support for iOS icon variations and adjusted iOS tablet support.
    - Introduced a new build configuration for iOS simulator in eas.json to enhance development workflow.
    
    This update enhances the overall configuration management and prepares the app for better deployment scenarios.

--- app.config.js
+++ app.config.js
@@ -2,79 +2,84 @@ const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 const getUniqueIdentifier = () => {
-	if (IS_DEV) {
-		return "com.giggzze.libertas.dev";
-	}
+  if (IS_DEV) {
+    return "com.giggzze.libertas.dev";
+  }
 
-	if (IS_PREVIEW) {
-		return "com.giggzze.libertas.preview";
-	}
+  if (IS_PREVIEW) {
+    return "com.giggzze.libertas.preview";
+  }
 
-	return "com.giggzze.libertas";
+  return "com.giggzze.libertas";
 };
 
 const getAppName = () => {
-	if (IS_DEV) {
-		return "Libertas (Dev)";
-	}
+  if (IS_DEV) {
+    return "Libertas (Dev)";
+  }
 
-	if (IS_PREVIEW) {
-		return "Libertas (Preview)";
-	}
+  if (IS_PREVIEW) {
+    return "Libertas (Preview)";
+  }
 
-	return "Libertas";
+  return "Libertas";
 };
 
 export default ({ config }) => ({
-	...config,
-	name: getAppName(),
-	slug: "libertas",
-	version: "1.0.0",
-	orientation: "portrait",
-	icon: "./assets/images/icon.png",
-	scheme: "libertas",
-	userInterfaceStyle: "automatic",
-	newArchEnabled: true,
-	ios: {
-		supportsTablet: true,
-		bundleIdentifier: getUniqueIdentifier(),
-		infoPlist: {
-			ITSAppUsesNonExemptEncryption: false,
-		},
-	},
-	android: {
-		adaptiveIcon: {
-			foregroundImage: "./assets/images/adaptive-icon.png",
-			backgroundColor: "#ffffff",
-		},
-		edgeToEdgeEnabled: true,
-		package: getUniqueIdentifier(),
-	},
-	web: {
-		bundler: "metro",
-		output: "static",
-		favicon: "./assets/images/favicon.png",
-	},
-	plugins: [
-		"expo-router",
-		[
-			"expo-splash-screen",
-			{
-				image: "./assets/images/splash-icon.png",
-				imageWidth: 200,
-				resizeMode: "contain",
-				backgroundColor: "#ffffff",
-			},
-		],
-	],
-	experiments: {
-		typedRoutes: true,
-	},
-	extra: {
-		router: {},
-		eas: {
-			projectId: "465b9a55-4ffb-4b8b-b25f-c1bac6941529",
-		},
-	},
-	owner: "giggzze",
+  ...config,
+  name: getAppName(),
+  slug: "libertas",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "libertas",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: getUniqueIdentifier(),
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
+    icon: {
+      dark: "./assets/images/ios-light.png",
+      light: "./assets/images/ios-light.png",
+      tinted: "./assets/images/ios-light.png",
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    package: getUniqueIdentifier(),
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon-light.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#343434",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "465b9a55-4ffb-4b8b-b25f-c1bac6941529",
+    },
+  },
+  owner: "giggzze",
 });
