acef2ecb943367012064fbd05de07b0f178262bf
giggzze <giggzze@live.co.uk>
2025-09-13 08:53:00 +0100

    created ios version

---
+++ Libertas/Libertas/LibertasApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibertasApp.swift
+//  Libertas
+//
+//  Created by Rueshelle Ferguson on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LibertasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
